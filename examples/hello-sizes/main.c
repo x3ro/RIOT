15a6a2379355013027a94c38d90c74591b1e4eaf
Lucas Jenss <lucas@x3ro.de>
2016-03-01 12:42:41 +0100

    [ma/upperlayer] Add stuff

---
+++ examples/hello-sizes/main.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2014 Freie Universität Berlin
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Kaspar Schleiser <kaspar@schleiser.de>
+ * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <diskio.h>
+#include <assert.h>
+
+#include "storage/ftl.h"
+#include "storage/osl.h"
+
+int write(const unsigned char *buffer, uint32_t page, uint32_t offset, uint16_t length);
+
+int read(unsigned char *buffer, uint32_t page, uint32_t offset, uint16_t length);
+
+int erase(uint32_t block);
+
+unsigned char subpage_buffer[256];
+unsigned char ecc_buffer[6];
+
+ftl_device_s device = {
+    .total_pages = 32768,
+    .page_size = 512,
+    .subpage_size = 256,
+    .pages_per_block = 1024,
+    .ecc_size = 6,
+    .partition_count = 2,
+
+    ._write = write,
+    ._read = read,
+    ._erase = erase,
+    ._bulk_erase = NULL,
+
+    ._subpage_buffer = subpage_buffer,
+    ._ecc_buffer = ecc_buffer
+};
+
+
+ftl_partition_s index_partition = {
+    .device = &device,
+    .base_offset = 0,
+    .size = 4
+};
+
+
+ftl_partition_s data_partition = {
+    .device = &device,
+    .base_offset = 4,
+    .size = 27
+};
+
+ftl_partition_s *partitions[] = {
+    &index_partition,
+    &data_partition
+};
+
+int write(const unsigned char *buffer, uint32_t page, uint32_t offset, uint16_t length) {
+    assert__(offset == 0);
+    assert__(length == device.subpage_size);
+    int ret = MCI_write(buffer, page, 1);
+    return ret;
+}
+
+int read(unsigned char *buffer, uint32_t page, uint32_t offset, uint16_t length) {
+    assert__(offset == 0);
+    assert__(length == device.subpage_size);
+    int ret = MCI_read(buffer, page, 1);
+    return ret;
+}
+
+int erase(uint32_t block) {
+    unsigned int buff[2];
+    buff[0] = block * device.pages_per_block;
+    buff[1] = (block + 1) * device.pages_per_block - 1;
+    int ret = MCI_ioctl(CTRL_ERASE_SECTOR, &buff);
+    return ret;
+}
+
+osl_s osl;
+
+int main(void)
+{
+//    int ret;
+
+    device.partitions = partitions;
+    printf("osl_s: %d\n", sizeof(osl_s));
+    printf("osl_object: %d\n", sizeof(osl_object_s));
+    printf("osl_record_cache_s: %d\n", sizeof(osl_record_cache_s));
+
+    return 0;
+}
